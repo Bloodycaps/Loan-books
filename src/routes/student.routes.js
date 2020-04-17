92e59fc9f570fe9e5d90a3a3608de6432c3fcb7d
Danilo Cortes <dacortes63@misena.edu.co>
2020-04-17 11:40:47 -0500

    chore: se realiza el enrutamiento para la pagina principal de estudiantes y se añade ruta para editarlos

--- src/routes/student.routes.js
+++ src/routes/student.routes.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../controllers/database");
 
-router.get("/", (req, res) => {
-  res.render("student/index");
+router.get("/edit/:id", async (req, res) => {
+  const { id } = req.params;
+  const estudiante = await db.query("SELECT * FROM estudiante WHERE IdLector = ?", [
+    id,
+  ]);
+  res.render("student/edit", {estudiante: estudiante[0]});
+});
+
+router.get("/", async (req, res) => {
+  const estudiantes = await db.query("SELECT * FROM estudiante");
+  res.render("student/index", { estudiantes });
 });
 
 module.exports = router;
