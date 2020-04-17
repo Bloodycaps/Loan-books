497b6c9a1afdaee6d2d1e94176990b86c88e0d43
Danilo Cortes <dacortes63@misena.edu.co>
2020-04-17 17:16:06 -0500

    chore: se cambia el modulo de mysql por el modulo de postgreSQL

--- src/controllers/database.js
+++ src/controllers/database.js
@@ -1,26 +1,7 @@
-const mysql = require("mysql");
+const { Pool } = require("pg");
 const { promisify } = require("util");
 const { database } = require("./keys");
 
-const db = mysql.createPool(database);
-
-db.getConnection((err, connection) => {
-  if (err) {
-    if (err.code === "PROTOCOL_CONNECTION_LOST") {
-      console.error("CONECCION CON LA BASE DE DATOS CERRADA");
-    }
-    if (err.code === "ER_CONT_COUNT_ERROR") {
-      console.error("LA BASE DE DATOS TIENE DEMASIADAS CONECCIONES");
-    }
-    if (err.code === "ECONNREFUSED") {
-      console.error("CONECCION A BASE DE DATOS RECHAZADA");
-    }
-  }
-  if (connection) connection.release();
-  console.log("DB EN LINEA");
-  return;
-});
-
-db.query = promisify(db.query);
+const db = new Pool(database);
 
 module.exports = db;
