101864eddc7e6d81e0e5b7289865020739674979
Danilo Cortes <dacortes63@misena.edu.co>
2020-04-17 14:54:59 -0500

    chore: se añaden las rutas y las vistas para completar el modulo de estudiantes

--- src/routes/student.routes.js
+++ src/routes/student.routes.js
@@ -2,12 +2,53 @@ const express = require("express");
 const router = express.Router();
 const db = require("../controllers/database");
 
+router.get("/new", async (req, res) => {
+  res.render("student/new");
+});
+
+router.post("/new", async (req, res) => {
+  const { ci, nombre, direccion, carrera, edad } = req.body;
+  const newEstudiante = {
+    CI: ci,
+    Nombre: nombre,
+    Direccion: direccion,
+    Carrera: carrera,
+    Edad: edad,
+  };
+  await db.query("INSERT INTO estudiante SET ?", [newEstudiante]);
+  res.redirect("/estudiantes");
+});
+
 router.get("/edit/:id", async (req, res) => {
   const { id } = req.params;
-  const estudiante = await db.query("SELECT * FROM estudiante WHERE IdLector = ?", [
+  const estudiante = await db.query(
+    "SELECT * FROM estudiante WHERE IdLector = ?",
+    [id]
+  );
+  res.render("student/edit", { estudiante: estudiante[0] });
+});
+
+router.post("/edit/:id", async (req, res) => {
+  const { id } = req.params;
+  const { ci, nombre, direccion, carrera, edad } = req.body;
+  const newEstudiante = {
+    CI: ci,
+    Nombre: nombre,
+    Direccion: direccion,
+    Carrera: carrera,
+    Edad: edad,
+  };
+  await db.query("UPDATE estudiante SET ? WHERE IdLector = ?", [
+    newEstudiante,
     id,
   ]);
-  res.render("student/edit", {estudiante: estudiante[0]});
+  res.redirect("/estudiantes");
+});
+
+router.get("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+  await db.query("DELETE FROM estudiante WHERE IdLector = ?", [id]);
+  res.redirect("/estudiantes");
 });
 
 router.get("/", async (req, res) => {
