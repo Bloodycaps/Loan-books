9cbcd888a1e6d8d0ecb12489ddff8be051441a6b
Danilo Cortes <dacortes63@misena.edu.co>
2020-04-17 16:51:45 -0500

    chore: se cambian las credenciales de la base de datos para que apunten al hosting

--- src/controllers/keys.js
+++ src/controllers/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     database: {
-        host: 'localhost',
-        user:"root",
-        password: "12345",
-        database: "biblioteca"
+        host: 'ec2-18-233-137-77.compute-1.amazonaws.com',
+        user:"vstkjjyjjhcrzq",
+        password: "6db53130856510f1dc18028bf063245c9ef2b203e08bdc694c3e1b35d12b176a",
+        database: "d2mk2q06e1p32e"
     }
 }
