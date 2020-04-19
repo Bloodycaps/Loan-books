4eebd96cfd33a30bc155a37cc4f04d96abb9cc83
Danilo Cortes <dacortes63@misena.edu.co>
2020-04-18 23:40:15 -0500

    chore: se cambian las keys de acceso a la base de datos local por las de la nube

--- src/controllers/keys.js
+++ src/controllers/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     database: {
-        host: 'localhost',
-        user:"postgres",
-        password: "12345",
-        database: "biblioteca"
+        host: 'ec2-34-233-186-251.compute-1.amazonaws.com',
+        user:"tfejttqgvntdgk",
+        password: "74c2e00edd0dc9d82dc1be07847c333897c51a40f135c3a230719ad2759e0bc4",
+        database: "dd64tofu455p28"
     }
 }
