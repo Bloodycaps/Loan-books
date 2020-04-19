15864f8c4ace412df6b89f1b481fbc987e9b27be
Danilo Cortes <dacortes63@misena.edu.co>
2020-04-18 23:38:00 -0500

    chore: se realiza cambio de bases de datos de MySQL a Postgres

--- src/controllers/database.js
+++ src/controllers/database.js
@@ -4,4 +4,23 @@ const { database } = require("./keys");
 
 const db = new Pool(database);
 
+db.connect((err, connection) => {
+    if (err){
+        if (err.code === 'PROTOCOL_CONNECTION_LOST'){
+            console.error('DATABASE CONNECTION WAS CLOSED');
+        }
+        if (err.code === 'ER_CON_COUNT_ERROR'){
+            console.error('DATABASE HAVE MANY CONNECTIONS');
+        }
+        if (err.code === 'ECONNREFUSED'){
+            console.error('DATABASE CONNECTION WAS REFUSED');
+        }
+    }
+
+    if (connection) connection.release();
+    console.log('DB IN LINE');
+    return;
+});
+db.query = promisify(db.query);
+
 module.exports = db;
--- src/routes/student.routes.js
+++ src/routes/student.routes.js
@@ -22,23 +22,24 @@ router.post("/new", async (req, res) => {
 router.get("/edit/:id", async (req, res) => {
   const { id } = req.params;
   const estudiante = await db.query(
-    "SELECT * FROM estudiante WHERE IdLector = ?",
+    "SELECT * FROM estudiante WHERE idlector = $1",
     [id]
   );
-  res.render("student/edit", { estudiante: estudiante[0] });
+  console.log(estudiante.rows);
+  res.render("student/edit", { estudiante: estudiante.rows[0] });
 });
 
 router.post("/edit/:id", async (req, res) => {
   const { id } = req.params;
   const { ci, nombre, direccion, carrera, edad } = req.body;
   const newEstudiante = {
-    CI: ci,
-    Nombre: nombre,
-    Direccion: direccion,
-    Carrera: carrera,
-    Edad: edad,
+    ci,
+    nombre,
+    direccion,
+    carrera,
+    edad,
   };
-  await db.query("UPDATE estudiante SET ? WHERE IdLector = ?", [
+  await db.query("UPDATE estudiante SET $1 WHERE idlector = $2", [
     newEstudiante,
     id,
   ]);
@@ -47,13 +48,13 @@ router.post("/edit/:id", async (req, res) => {
 
 router.get("/delete/:id", async (req, res) => {
   const { id } = req.params;
-  await db.query("DELETE FROM estudiante WHERE IdLector = ?", [id]);
+  await db.query("DELETE FROM estudiante WHERE IdLector = $1", [id]);
   res.redirect("/estudiantes");
 });
 
 router.get("/", async (req, res) => {
   const estudiantes = await db.query("SELECT * FROM estudiante");
-  res.render("student/index", { estudiantes });
+  res.render("student/index", { estudiantes: estudiantes.rows });
 });
 
 module.exports = router;
