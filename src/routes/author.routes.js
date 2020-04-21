644da37e4d5b3af29acbda43c5575ff5c89b9ecc
Danilo Cortes <dacortes63@misena.edu.co>
2020-04-21 14:30:14 -0500

    chore: se realiza el enrutamiento para las vista de nuevos autores y la correcciones en la vista principal de autores

--- src/routes/author.routes.js
+++ src/routes/author.routes.js
@@ -2,9 +2,28 @@ const express = require("express");
 const router = express.Router();
 const db = require("../controllers/database");
 
+router.get("/new", (req, res) => {
+  res.render("authors/new");
+});
+
+router.post("/new", async (req, res) => {
+  const { nombre, nacionalidad } = req.body;
+  await db.query("INSERT INTO autor ( nombre, nacionalidad) VALUES ($1, $2) ", [
+    nombre,
+    nacionalidad,
+  ]);
+  res.redirect("/autores");
+});
+
+router.get("/edit/:id", async (req, res) => {
+  const { id } = req.params;
+  const autor = await db.query("SELECT * FROM autor WHERE idautor = $1", [id]);
+  res.render("authors/edit",{autor: autor.rows[0]});
+});
+
 router.get("/", async (req, res) => {
   const autores = await db.query("SELECT * FROM autor");
-  res.render("authors/index",{autores: autores.rows});
+  res.render("authors/index", { autores: autores.rows });
 });
 
 module.exports = router;
