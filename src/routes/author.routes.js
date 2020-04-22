1236926d79424ceeb3ba5706793e4320e4da92bf
Danilo Cortes <dacortes63@misena.edu.co>
2020-04-22 16:52:50 -0500

    chore: se realiza el enrutamiento para editar y eliminar autores con sus respectivas rutas segun se requiere

--- src/routes/author.routes.js
+++ src/routes/author.routes.js
@@ -18,7 +18,23 @@ router.post("/new", async (req, res) => {
 router.get("/edit/:id", async (req, res) => {
   const { id } = req.params;
   const autor = await db.query("SELECT * FROM autor WHERE idautor = $1", [id]);
-  res.render("authors/edit",{autor: autor.rows[0]});
+  res.render("authors/edit", { autor: autor.rows[0] });
+});
+
+router.post("/edit/:id", async (req, res) => {
+  const { id } = req.params;
+  const { nombre, nacionalidad } = req.body;
+  await db.query(
+    "UPDATE autor SET nombre = $1, nacionalidad = $2 WHERE idautor = $3",
+    [nombre, nacionalidad, id]
+  );
+  res.redirect("/autores");
+});
+
+router.get("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+  await db.query("DELETE FROM autor WHERE idautor = $1", [id]);
+  res.redirect("/autores");
 });
 
 router.get("/", async (req, res) => {
